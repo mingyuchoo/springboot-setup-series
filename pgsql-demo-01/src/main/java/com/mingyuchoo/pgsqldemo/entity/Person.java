5450088c968227cc070c4ebc3a0920e95c73148b
mingyuchoo <mingyuchoo@gmail.com>
2023-11-25 04:46:50 -0500

    update

--- pgsql-demo-01/src/main/java/com/mingyuchoo/pgsqldemo/entity/Person.java
+++ pgsql-demo-01/src/main/java/com/mingyuchoo/pgsqldemo/entity/Person.java
@@ -1,12 +1,12 @@
 package com.mingyuchoo.pgsqldemo.entity;
 
 import java.io.Serializable;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
